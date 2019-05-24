8718e41c05b5f3e6dbb79c70b39d87585cade515
VANDRIANAMW <VANDRIANAMW@gmail.com>
2019-05-24 23:06:44 +0000

    Ejercicio 2.4

--- exercises/02.4-jumbotron/index.js
+++ exercises/02.4-jumbotron/index.js
@@ -4,19 +4,31 @@ import PropType from "prop-types";
 
 const Jumbotron = props => {
 	//here you have to return expected html using the properties being passed to the component
+	return (
+		<div className="jumbotron m-5">
+			<h1 className="display-4">{props.title}</h1>
+			<p className="lead">{props.description}</p>
+			<a className="btn btn-primary btn-lg" href={props.buttonUrl} role="button">
+				{props.label}
+			</a>
+		</div>
+	);
 };
 
 Jumbotron.propTypes = {
-    //proptypes here
+	//proptypes here
+	title: PropType.string,
+	description: PropType.string,
+	buttonUrl: PropType.string,
+	label: PropType.string
 };
 
 ReactDOM.render(
 	<Jumbotron
 		title="Welcome to react"
 		description="React is the most popular rendering library in the world"
-		buttonLabel="Go to the official website"
-		buttonURL="https://reactjs.org/"
+		label="Go to the official website"
+		buttonUrl="https://reactjs.org/"
 	/>,
-
 	document.querySelector("#myDiv")
 );
